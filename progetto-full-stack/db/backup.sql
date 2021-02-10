07bfa8eebae55c7b5722c2763e25d9bc5448b63f
marco.bertelli-at-170187898933 <marcobert37@gmail.com>
2021-02-10 12:49:11 +0100

    dump

--- progetto-full-stack/db/backup.sql
+++ progetto-full-stack/db/backup.sql
@@ -139,6 +139,31 @@ INSERT INTO `categorie` VALUES (1,'cat1'),(3,'cat3'),(2,'prova'),(4,'prova con a
 UNLOCK TABLES;
 
 --
+-- Table structure for table `citta`
+--
+
+DROP TABLE IF EXISTS `citta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `citta` (
+  `id` int(11) NOT NULL,
+  `descrizione` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`),
+  FULLTEXT KEY `descrizione` (`descrizione`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `citta`
+--
+
+LOCK TABLES `citta` WRITE;
+/*!40000 ALTER TABLE `citta` DISABLE KEYS */;
+INSERT INTO `citta` VALUES (1,'prova'),(2,'brescia');
+/*!40000 ALTER TABLE `citta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `clienti`
 --
 
@@ -179,7 +204,7 @@ CREATE TABLE `ordini` (
   `cliente` int(11) NOT NULL,
   `totale` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -188,7 +213,7 @@ CREATE TABLE `ordini` (
 
 LOCK TABLES `ordini` WRITE;
 /*!40000 ALTER TABLE `ordini` DISABLE KEYS */;
-INSERT INTO `ordini` VALUES (1,'AB123','2020-02-04',2,167.4),(2,'AC123','2020-02-01',1,114.7);
+INSERT INTO `ordini` VALUES (1,'AB123','2020-02-04',12,167.4),(2,'AC123','2020-02-01',1,114.7);
 /*!40000 ALTER TABLE `ordini` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -229,4 +254,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-02-10 11:10:19
+-- Dump completed on 2021-02-10 11:49:02
