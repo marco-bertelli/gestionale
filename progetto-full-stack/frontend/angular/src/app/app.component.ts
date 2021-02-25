e7fb5c38d4141ea581b8f6e1d3eeb06fdfdec3e0
naniandrea2000 <andreanani1400@gmail.com>
2021-02-25 18:04:43 +0100

    Login visaiva fatta manca responsive background

--- progetto-full-stack/frontend/angular/src/app/app.component.ts
+++ progetto-full-stack/frontend/angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  link : string = "";
+  
+
+  constructor(private router: Router, location: Location){
+          this.link=location.path();
+          console.log(this.link);
+    }
 }
