fef1d1bde17fb076d6d35b320c616bdee34cf6b0
naniandrea2000 <andreanani1400@gmail.com>
2021-02-25 16:26:32 +0100

    Aggiunta login al routing module

--- progetto-full-stack/frontend/angular/src/app/app-routing.module.ts
+++ progetto-full-stack/frontend/angular/src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ const routes: Routes = [
   { path: 'articoli', loadChildren: () => import('./features/articoli/articoli.module').then(m => m.ArticoliModule) },
   { path: 'clienti', loadChildren: () => import('./features/clienti/clienti.module').then(m => m.ClientiModule) },
   { path: 'ordini', loadChildren: () => import('./features/ordini/ordini.module').then(m => m.OrdiniModule) },
-  { path: 'fatture', loadChildren: () => import('./features/fatture/fatture.module').then(m => m.FattureModule) }
+  { path: 'fatture', loadChildren: () => import('./features/fatture/fatture.module').then(m => m.FattureModule)},
+  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) 
+}
 
 ];
 
